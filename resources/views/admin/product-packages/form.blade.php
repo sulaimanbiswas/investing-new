747dab3ff139a1a0a8839d528c30731bb6e255f7
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2025-12-15 18:31:38 +0600

    refactor: Remove Select2 integration and related refresh functionality from product package form

--- resources/views/admin/product-packages/form.blade.php
+++ resources/views/admin/product-packages/form.blade.php
@@ -1,7 +1,5 @@
 @csrf
-@push('styles')
-    <link href="{{ asset('admin/vendor/select2/css/select2.min.css') }}" rel="stylesheet" />
-@endpush
+
 <div class="row">
     <div class="col-md-3 mb-3">
         <label class="form-label">Order Set <span class="text-danger">*</span></label>
@@ -127,22 +125,7 @@
 </div>
 
 @push('scripts')
-    <script src="{{ asset('admin/vendor/select2/js/select2.full.min.js') }}"></script>
     <script>
-        function refreshProductSelect2() {
-            if (!window.jQuery) return;
-            $('.product-select').each(function () {
-                var $el = $(this);
-                if ($el.hasClass('select2-hidden-accessible')) {
-                    $el.select2('destroy');
-                }
-                $el.select2({
-                    placeholder: 'Select Product',
-                    width: '100%'
-                });
-            });
-        }
-
         let productIndex = {{ isset($productPackage) && $productPackage->productPackageItems->count() > 0 ? $productPackage->productPackageItems->count() : 1 }};
         let availableProducts = [];
         const editMode = {{ isset($productPackage) ? 'true' : 'false' }};
@@ -253,8 +236,6 @@
                     select.appendChild(option);
                 });
             });
-
-            refreshProductSelect2();
         }
 
         // Add product row
@@ -311,7 +292,6 @@
 
             // Update all product selects to filter out already selected products
             updateAllProductSelects();
-            refreshProductSelect2();
         });
 
         // Remove product row
@@ -450,7 +430,6 @@
             }
 
             calculateTotals();
-            refreshProductSelect2();
         });
     </script>
 @endpush
