f2481c633068d2e69becb005980e694bc8076fbf
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2025-12-23 22:10:03 +0600

    feat: Improve logo alignment and styling on admin login page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -11,10 +11,10 @@
             <div class="row no-gutters">
               <div class="col-xl-12">
                 <div class="auth-form">
-                  <div class="text-center mb-3">
+                  <div class="d-flex justify-content-center align-items-center mb-3">
                     <a href="{{ route('admin.dashboard') }}" class="brand-logo">
                       @if(setting('logo_path'))
-                        <dev class='d-flex  align-items-center'>
+                        <dev class='d-flex justify-content-center align-items-center'>
                           <img src="{{ asset(setting('logo_path')) }}" alt="Logo"
                             style="max-height: 40px; max-width: 40px; object-fit: contain;">
                           <span class="text-dark brand-title text-primary" style="font-size: 20px; font-weight: 600;">
