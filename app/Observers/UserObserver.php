85f7cef0a5c082ac5adf4ff3762b50dd161f2f68
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2026-03-15 13:39:30 +0600

    feat: Add notification for new user registration in UserObserver

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Models\Notification;
 use App\Models\User;
 
 class UserObserver
@@ -22,7 +23,20 @@ class UserObserver
      */
     public function created(User $user): void
     {
-        //
+        Notification::create([
+            'user_id' => $user->id,
+            'type' => 'user_registered',
+            'title' => 'New User Registration',
+            'message' => 'New user ' . ($user->name ?: $user->username ?: 'Unknown') . ' has created an account.',
+            'data' => [
+                'registered_user_id' => $user->id,
+                'registered_user_name' => $user->name,
+                'registered_username' => $user->username,
+                'registered_email' => $user->email,
+                'registered_phone' => $user->phone,
+            ],
+            'is_for_admin' => true,
+        ]);
     }
 
     /**
